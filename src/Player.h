299c7ab9916f0761c96734df9e41a8c49c47dc36
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2020-09-10 22:58:16 -0400

    added and implemented several functions in Player class

--- src/Player.h
+++ src/Player.h
@@ -13,15 +13,29 @@ class Player {
 public:
     Player();
     void nextMove();
-    void takeOneCard();
+    void takeOneCard(Card card);
+    bool setTrumpSuit(Suit trumpSuit);
 private:
+    Suit trumpSuit; 
     std::vector<Card> hand;
     std::vector<Card> meldPile;
     std::vector<Card> capturePile;
+    std::vector<std::vector<Card>> meldsUsed;
     Card playFromHand();
     void playMeld();
     void suggestNextMeld();
     void suggestNextThrow();
+    void getPotentialMelds();
+    size_t getMeldPoints();
+    bool isValidMeld(std::vector<Card> cards, Meld *whatMeld);
+    bool hasMeldBeenUsed();
+    bool isMeldValid();
+    bool isDix(Card card);
+    bool isMarriage(std::vector<Card> cards);
+    bool isRoyalMarriage(std::vector<Card> cards);
+    bool isPinochle(std::vector<Card> cards);
+    bool isFours(std::vector<Card> cards);
+    bool isFlush(std::vector<Card> cards);
 
 };
 
